96ffdb1ad7834bbe605acd8ca53bc926395c7ba7
Lavínia <lavizsenai@gmail.com>
2025-11-14 08:40:10 -0300

    Aula 11 finalizada

--- Aula 11/js/app.js
+++ Aula 11/js/app.js
@@ -1,33 +1,29 @@
-// ======== CONEXÃO COM OS ELEMENTOS HTML ========
-// Pega os elementos do HTML pelo ID
-const awakenButton = document.getElementById("awakenButton");
+const bgAudio = document.getElementById('bgAudio');
+const awakenButton = document.getElementById('awakenButton');
 const druidNameInput = document.getElementById("druidNameInput");
 const forestResponse = document.getElementById("forestResponse");
 
-// ======== FEITIÇO DE ESCUTA (Evento de clique) ========
 awakenButton.addEventListener("click", () => {
-  const nome = druidNameInput.value.trim(); // Pega o nome digitado e remove espaços extras
+  bgAudio.play().catch(err => {
+    console.error('Não foi possível reproduzir o áudio:', err);
+  });
+  const nome = druidNameInput.value.trim();
 
-  // Se o campo estiver vazio
   if (nome === "") {
     forestResponse.textContent = "🍃 O vento espera um nome... sussurre-o, jovem druida.";
     forestResponse.classList.remove("awakened");
     return;
   }
 
-  // ======== RESPOSTA PERSONALIZADA ========
   let mensagem;
 
-  // Desafio de Bronze 🥉 — nome especial “Veridian”
   if (nome.toLowerCase() === "veridian") {
     mensagem = "🌲 A Árvore-Mãe desperta em alegria! 'Veridian... o Guardião retornou!' 🌿";
   } else {
     mensagem = `✨ A floresta ouviu seu chamado, druida ${nome}! As folhas sussurram em gratidão. 🌳`;
   }
 
-  // Exibe a mensagem na div
   forestResponse.textContent = mensagem;
 
-  // Ativa a animação mágica
   forestResponse.classList.add("awakened");
 });
